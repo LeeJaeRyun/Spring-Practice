c016c1c648af634b00f37e8c679d124fbc5ee9a3
LeeJaeRyun <pinixs3@naver.com>
2025-02-21 00:53:52 +0900

    Lombok 라이브러리 적용

--- core/src/main/java/hello/core/order/OrderSerivceImpl.java
+++ core/src/main/java/hello/core/order/OrderSerivceImpl.java
@@ -6,23 +6,18 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor//final이 붙은 필드를 모아서 자동으로 생성자를 만들어줌
 public class OrderSerivceImpl implements OrderService {
 
+    //final은 생성자 주입에서만 가능함
+    //final키워드는 생성자에서 혹시라도 값이 설정되지 않는 오류를 컴파일 시점에 막아준다
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderSerivceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("1. OrderSerivceImpl");
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
