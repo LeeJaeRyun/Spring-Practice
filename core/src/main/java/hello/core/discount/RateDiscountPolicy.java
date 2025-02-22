606555a386d35f9752175ab8f06e3d6889dfe833
LeeJaeRyun <pinixs3@naver.com>
2025-02-22 23:02:21 +0900

    조회한 빈이 모두 필요할 경우 - 전략패턴

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -7,9 +8,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary//제일 자주쓰는 방법
-//Primary보다 Qualifier이 우선 순위가 더 높음
-//Spring은 좁은 범위의 선택권이 우선 순위가 더 높음
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
