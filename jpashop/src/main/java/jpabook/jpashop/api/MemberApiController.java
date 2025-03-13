67275da21051fc73376c1244effa3b3ee680a849
LeeJaeRyun <pinixs3@naver.com>
2025-03-13 18:27:44 +0900

    단순 회원 조회 API

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -10,12 +10,48 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 //ResponseBody => 데이터자체를 json으로 바로 보내자 할때 사용
 @RestController//ResponseBody + Controller => RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> MembersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(),collect);
+    }
+    //스트림(Stream)은 데이터를 하나씩 처리할 수 있는 기능을 제공하는 도구
+    //각 Member 객체에서 getName()을 꺼내서 새로운 MemberDto 객체를 생성
+    //.map()을 통해 변환된 데이터들을 리스트(List<MemberDto>)로 다시 모음
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+
 
     //@RequestBody → 클라이언트가 보낸 JSON 데이터를 Member 객체로 변환
     @PostMapping("/api/v1/members")
