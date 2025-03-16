e3a1be00e41e5fc008d5180b5f03e18f1926e57e
LeeJaeRyun <pinixs3@naver.com>
2025-03-16 21:52:20 +0900

    간단한 주문 조회 v1: 엔티티를 직접 노출할 경우 문제

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
 import lombok.*;
@@ -17,11 +18,12 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
-    private int orderPrice; //주문 가격
+    private int orderPrice; //주문 가격z
     private int count; //주문 수량
 
 
